de945732895623eaa966c6d7e39ff332a8012627
MinniZaid-dev <minizaid007@gmail.com>
2026-09-15 21:40:19 +0530

    Solved lecture 1 question 1

---
+++ 01_basics/lecture_01/questions.py
@@ -0,0 +1,7 @@
+# Question 1
+# Write a Program to input 2 numbers & print their sum.
+
+first = int(input("First number: "))
+second = int(input("Second number: "))
+
+print("Sum =", first + second)
