eeb3213e3a4638a1491c5deeb6b3aa6cee3d5cf6
MinniZaid-dev <minizaid007@gmail.com>
2026-09-17 22:52:59 +0530

    Completed lecture 1 practice questions

--- 01_basics/lecture_01/practice.py
+++ 01_basics/lecture_01/practice.py
@@ -2,3 +2,16 @@ first = int(input("first number:"))
 second = int(input("second number:"))
 
 print("sum =", first+second)
+
+side = float(input("Enter square side: "))
+print("Area =",side*side)
+
+a = float(input("First number: "))
+b = float(input("Second number: "))
+
+print("Average =", (a + b) / 2)
+
+a = int(input("Enter first : "))
+b = int(input("Enter second: "))
+
+print(a >= b)
--- 01_basics/lecture_01/questions.py
+++ 01_basics/lecture_01/questions.py
@@ -5,3 +5,27 @@ first = int(input("First number: "))
 second = int(input("Second number: "))
 
 print("Sum =", first + second)
+
+# Question 2
+# Write a program to input side of a square & print its area.
+
+side = float(input("Enter square side: "))
+print("Area =",side*side)
+
+# Question 3
+# WAP to input 2 floating point numbers & print their average.
+
+a = float(input("First number: "))
+b = float(input("Second number: "))
+
+print("Average =", (a + b) / 2)
+
+
+# Question 4
+# WAP to input 2 int numbers, a and b.
+# Print True if a is greater than or equal to b. If not print False.
+
+a = int(input("Enter first : "))
+b = int(input("Enter second: "))
+
+print(a >= b)
