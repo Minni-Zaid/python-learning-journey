f3aeae027370b6c10fe7dee1d31995898790a233
MinniZaid-dev <minizaid007@gmail.com>
2026-09-20 17:39:37 +0530

    Completed Lecture 2 practice

---
+++ 01_basics/lecture_02/practice.py
@@ -0,0 +1,6 @@
+name = input("enter your name:")
+print("length of your name is",len(name))
+
+
+str = "hi , iam the $ symbol $12.22"
+print(str.count("$"))   
---
+++ 01_basics/lecture_02/question.py
@@ -0,0 +1,12 @@
+#QN 1
+#write a program to input users frist name and print its length.
+
+name = input("enter your name:")
+print("length of your name is",len(name))
+
+
+# QN2
+# WAP to find the occurrence of "$" in a string
+
+str = "hi , iam  $ the $ symbol $12.22"
+print(str.count("$"))
