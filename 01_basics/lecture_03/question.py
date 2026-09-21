13fcd185144fe8b4c3253c6e20687a99f87c13c0
MinniZaid-dev <minizaid007@gmail.com>
2026-09-21 21:55:50 +0530

    updated lecture 3 question

--- 01_basics/lecture_03/question.py
+++ 01_basics/lecture_03/question.py
@@ -1,34 +1,34 @@
-#QN1
+QN1
 
-#write a program to ask the user to enter names
-#  of their 3 favorite movies and sotre them in a list
+write a program to ask the user to enter names
+ of their 3 favorite movies and sotre them in a list
 
-# movies =[]
-# mov1 = input("enter 1st movie:")
-# mov2 = input("enter 2nd movie:")
-# mov3 = input("enter 3rd movie:")
+movies =[]
+mov1 = input("enter 1st movie:")
+mov2 = input("enter 2nd movie:")
+mov3 = input("enter 3rd movie:")
 
-# movies.append(mov1)
-# movies.append(mov2)
-# movies.append(mov3)
+movies.append(mov1)
+movies.append(mov2)
+movies.append(mov3)
 
-# print(movies)
+print(movies)
  
-#another type
+another type
 
-# movies =[]
-# movies.append(input("enter 1st movie:"))
-# movies.append(input("enter 2nd movie:"))
-# movies.append(input("enter 3rd movie:"))
+movies =[]
+movies.append(input("enter 1st movie:"))
+movies.append(input("enter 2nd movie:"))
+movies.append(input("enter 3rd movie:"))
 
-# print(movies)
+print(movies)
 
 
 
-# QN 2
-# write  a program to check if a list conatain 
-# a palindrome of element (hint:use copy()method)
-#         "racecar"  [1,2,3,2,1]
+QN 2
+write  a program to check if a list conatain 
+a palindrome of element (hint:use copy()method)
+        "racecar"  [1,2,3,2,1]
 
 list1 = ["m","a","a","m"]
 
