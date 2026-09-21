b2fe3dc3cc576ad1a88f77f98fbe1af8d075436e
MinniZaid-dev <minizaid007@gmail.com>
2026-09-21 21:51:35 +0530

    Completed Lecture 3 practice

---
+++ 01_basics/lecture_03/practice.py
@@ -0,0 +1,69 @@
+marks = [65.7,34.6,89,70,39.8,45]
+print(marks)
+
+print(type(marks))
+
+print(len(marks))
+
+print(marks[0])
+print(marks[3])
+
+student = ["zaid" ,99, 20, "Germany"]
+print(student)
+
+print(type(student))
+
+print(len(student))
+
+print(student[2])
+
+student[0] ="Minni Mohamed Zaid"
+print(student)
+
+
+marks =[90,78,89,58,65]
+print(marks[:-5])
+
+print(marks[1:len(marks)])
+
+
+list = [20,40,60,45,]
+list.append(100)
+print(list)
+
+list =[ "orange", "Apple","banana","carrot","betroot"]
+
+# print(list.append(9))
+list.reverse()
+print(list)
+
+
+list =[5,6,7,3]
+list.insert(3,8)
+print(list)
+
+list =[5,6,7,10]
+list.pop(3)
+print(list)
+
+
+tup =(23,45,56,78)
+print(type(tup))
+print(tup[1])
+print(tup[3])
+
+tuple = (1,)
+print(tuple)
+print(type(tuple))
+
+tup = (2,3,4,5,6,)
+print(tup[1:3])
+
+tuple=(2,1,3,1)
+print(tuple.index(3))
+
+tuple=(2,1,3,1,3,6,3,6,3,7,3,6)
+print(tuple.count(6))
+
+
+
